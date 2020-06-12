00814f467b2320d02590be51e52b4271b9f296a4
Emmett Miller <emmett.miller93@gmail.com>
2020-06-11 19:29:31 -0700

    adds docstring in configs.yaml

--- configs.py
+++ configs.py
@@ -3,7 +3,12 @@ class InvalidConfig(Exception):
 
 
 def from_yaml(config_yaml):
-    assert config_yaml != None
+    """Import a YAML config file.
+
+    :return:
+        a dict loaded from the YAML file
+    """
+    assert config_yaml
 
     from yaml import safe_load
 
